2b9d45ea1683405cfccf2e67fc4f3495af0f2b89
jonasz7 <yonathansebastian71@gmail.com>
2023-05-25 12:50:37 +0700

    perbarui seeder

--- database/seeders/JadwalSeeder.php
+++ database/seeders/JadwalSeeder.php
@@ -16,20 +16,32 @@ class JadwalSeeder extends Seeder
     public function run()
     {
         $data = [
-            ['id_pengguna' => '1', 'goal' => 'Increase muscle size', 'tgl_mulai' => '2023-05-15', 'tgl_selesai' => '2023-05-20', 'sesi_latihan' => '4',
-            'jenis_latihan' => 'Push', 'status' => 'Proses'],
+            // ['id_pengguna' => '1', 'goal' => 'Increase muscle size', 'tgl_mulai' => '2023-05-15', 'tgl_selesai' => '2023-05-20', 'sesi_latihan' => '4',
+            // 'jenis_latihan' => 'Push', 'status' => 'Proses'],
 
-            ['id_pengguna' => '1', 'goal' => 'Lose body fat', 'tgl_mulai' => '2023-03-15', 'tgl_selesai' => '2023-04-15', 'sesi_latihan' => '6',
-            'jenis_latihan' => 'Pull', 'status' => 'Selesai'],
+            // ['id_pengguna' => '1', 'goal' => 'Lose body fat', 'tgl_mulai' => '2023-03-15', 'tgl_selesai' => '2023-04-15', 'sesi_latihan' => '6',
+            // 'jenis_latihan' => 'Pull', 'status' => 'Selesai'],
 
-            ['id_pengguna' => '1', 'goal' => 'Start an work out train', 'tgl_mulai' => '2023-02-10', 'tgl_selesai' => '2023-03-10', 'sesi_latihan' => '10',
-            'jenis_latihan' => 'Back', 'status' => 'Proses'],
+            // ['id_pengguna' => '1', 'goal' => 'Start an work out train', 'tgl_mulai' => '2023-02-10', 'tgl_selesai' => '2023-03-10', 'sesi_latihan' => '10',
+            // 'jenis_latihan' => 'Back', 'status' => 'Proses'],
 
-            ['id_pengguna' => '3', 'goal' => NULL, 'tgl_mulai' => '2023-06-15', 'tgl_selesai' => '2023-06-15', 'sesi_latihan' => '8',
-            'jenis_latihan' => 'Bicep', 'status' => 'Selesai'],
+            // ['id_pengguna' => '3', 'goal' => NULL, 'tgl_mulai' => '2023-06-15', 'tgl_selesai' => '2023-06-15', 'sesi_latihan' => '8',
+            // 'jenis_latihan' => 'Bicep', 'status' => 'Selesai'],
 
-            ['id_pengguna' => '4', 'goal' => NULL, 'tgl_mulai' => '2023-08-10', 'tgl_selesai' => '2023-09-10', 'sesi_latihan' => '12',
-            'jenis_latihan' => 'Tricep', 'status' => 'Proses'],
+            // ['id_pengguna' => '4', 'goal' => NULL, 'tgl_mulai' => '2023-08-10', 'tgl_selesai' => '2023-09-10', 'sesi_latihan' => '12',
+            // 'jenis_latihan' => 'Tricep', 'status' => 'Proses'],
+
+            ['id_pengguna' => '4', 'goal' => 'Increase muscle size', 'tgl_mulai' => '2023-03-02', 'tgl_selesai' => '2023-05-02', 'sesi_latihan' => '12',
+            'jenis_latihan' => 'Push, Pull', 'status' => 'Proses'],
+
+            ['id_pengguna' => '6', 'goal' => 'Lose Body Fat', 'tgl_mulai' => '2023-01-23', 'tgl_selesai' => '2023-02-23', 'sesi_latihan' => '12',
+            'jenis_latihan' => 'Push, Pull', 'status' => 'Proses'],
+
+            ['id_pengguna' => '7', 'goal' => 'Rehabilitate an injury', 'tgl_mulai' => '2023-01-09', 'tgl_selesai' => '2023-02-10', 'sesi_latihan' => '12',
+            'jenis_latihan' => 'Push, Pull', 'status' => 'Proses'],
+
+            ['id_pengguna' => '8', 'goal' => 'Lose Body Fat', 'tgl_mulai' => '2023-05-13', 'tgl_selesai' => '2023-06-13', 'sesi_latihan' => '12',
+            'jenis_latihan' => NULL, 'status' => 'Proses'],
         ];
 
         DB::table('jadwal')->insert($data);
--- database/seeders/PenggunaSeeder.php
+++ database/seeders/PenggunaSeeder.php
@@ -22,7 +22,7 @@ class PenggunaSeeder extends Seeder
             ['id' => '1', 'name' => 'Yonathan Sebastian', 'email' => 'jonas@gmail.com', 
             'password' => '$2y$10$w5h5YJE5U377q3WVukpj/u1UqKTJrz4Mh3fJEDw2aa36t2KbnFSd.', 'level' => 'Member',
             'tgl_lahir' => '2023-01-01', 'gender' => 'Pria', 'tlpn' => '0812-2323-4545', 
-            'alamat' => 'Jl. Tritunggal No.4, Salakan, Sorosutan, Kec. Umbulharjo, Kota Yogyakarta, Daerah Istimewa Yogyakarta 55162',
+            'alamat' => 'Jl. Tritunggal No.4',
             'kidal' => 'Tidak', 'lama_pnglmn' => '< 3 bulan',
             'created_at' => '2023-05-13 00:05:11', 'updated_at' => '2023-05-13 00:05:11'],
 
@@ -30,19 +30,33 @@ class PenggunaSeeder extends Seeder
             'password' => '$2y$10$yFaYvLUuf9y4RgVngfXrQe.DNAU/IhYk4.bd5eOMp56g1P.AYen5i', 'level' => 'Trainer',
             'created_at' => '2023-05-13 00:05:35', 'updated_at' => '2023-05-13 00:05:35'],
 
-            ['id' => '3', 'name' => 'Yosua Nissi', 'email' => 'yosua@gmail.com', 
-            'password' => '$2y$10$mmZiexZHMtx8eRl6JPcIeuwvOSvr8PDiw3HSRANRPMkkRGfiek64q', 'level' => 'Member',
-            'tgl_lahir' => '2022-02-02', 'gender' => 'Wanita', 'tlpn' => '0832-2222-4444', 
-            'alamat' => 'Jalan Pasar Telo',
-            'kidal' => 'Ya', 'lama_pnglmn' => '> 3 Bulan', 
+            // ['id' => '3', 'name' => 'Yosua Nissi', 'email' => 'yosua@gmail.com', 
+            // 'password' => '$2y$10$mmZiexZHMtx8eRl6JPcIeuwvOSvr8PDiw3HSRANRPMkkRGfiek64q', 'level' => 'Member',
+            // 'tgl_lahir' => '2022-02-02', 'gender' => 'Wanita', 'tlpn' => '0832-2222-4444', 
+            // 'alamat' => 'Jalan Pasar Telo',
+            // 'kidal' => 'Ya', 'lama_pnglmn' => '> 3 Bulan', 
+            // 'created_at' => '2023-05-13 08:47:22', 'updated_at' => '2023-05-13 08:47:22'],
+
+            ['id' => '4', 'name' => 'Indra', 'email' => 'indra@gmail.com', 
+            'password' => '$2y$10$.GmsU68hr8rngfW01N.2puEHhcmpDJ/E9byoL/XOdmLFVItfHchK.', 'level' => 'Member',
             'created_at' => '2023-05-13 08:47:22', 'updated_at' => '2023-05-13 08:47:22'],
 
-            ['id' => '4', 'name' => 'Marin', 'email' => 'marin@gmail.com', 
-            'password' => '$2y$10$lp.tScq8BtsdND2a/o22teY2d4wWs97kgpB9rcpJQ.sr/E6WUDpQS', 'level' => 'Member',
+            // ['id' => '5', 'name' => 'Okinawa', 'email' => 'oki@gmail.com', 
+            // 'password' => '$2y$10$JaExT9VAmugvdKd4FYNCZ.yO56HCXkV1IPWbudvGkGNrOSCNJHCnO', 'level' => 'Member',
+            // 'created_at' => '2023-05-13 08:47:22', 'updated_at' => '2023-05-13 08:47:22'],
+
+            ['id' => '6', 'name' => 'Atika', 'email' => 'atika@gmail.com', 
+            'password' => '$2y$10$AUkhdhUm6tDDZRllBs6mRu0JXILsSQWhZ4OZHEThXuckubYMMmSQe', 'level' => 'Member',
             'created_at' => '2023-05-13 08:47:22', 'updated_at' => '2023-05-13 08:47:22'],
 
-            ['id' => '5', 'name' => 'Okinawa', 'email' => 'oki@gmail.com', 
-            'password' => '$2y$10$JaExT9VAmugvdKd4FYNCZ.yO56HCXkV1IPWbudvGkGNrOSCNJHCnO', 'level' => 'Member',
+            ['id' => '7', 'name' => 'Bapak Sarjito', 'email' => 'sarjito@gmail.com', 
+            'password' => '$2y$10$loHGaQh1OW4U/wiSdeOi7emKrJi/GQyqoThqqVQVdATenHYE5209K', 'level' => 'Member',
+            'created_at' => '2023-05-13 08:47:22', 'updated_at' => '2023-05-13 08:47:22'],
+
+            ['id' => '8', 'name' => 'Zella', 'email' => 'zella@gmail.com', 
+            'password' => '$2y$10$rooZHo9qTLyFbdSnQS/6keOrrWzDohpL/rMF4ayBPLiwzCLYXTTCS', 'level' => 'Member',
+            'tlpn' => '0823-2908-5565', 
+            'alamat' => 'Gedong Kuning',
             'created_at' => '2023-05-13 08:47:22', 'updated_at' => '2023-05-13 08:47:22'],
         ];
         //DB::table('pengguna')->insert($data);
