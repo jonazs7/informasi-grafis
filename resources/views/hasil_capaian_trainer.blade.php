c0be7bdf307a5fd226ed6850ff41d32e097c6763
jonasz7 <yonathansebastian71@gmail.com>
2023-06-07 04:14:40 +0700

    coba rapiin dikit

--- resources/views/hasil_capaian_trainer.blade.php
+++ resources/views/hasil_capaian_trainer.blade.php
@@ -265,11 +265,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
                   <a type="button" class="btn btn-default btn-sm" href="{{ route('detailInfo', $capaian->id) }}">Detail Info</a> --}}
                   <div style="display: flex">
                     <a class="btn btn-app create-data-fisik" type="button" data-toggle="modal" 
-                    data-target="#modal-default-data-fisik" data-id="{{ $capaian->id }}" style="width: 10px; height: 10px; display: flex; justify-content: center; align-items: center;">
+                    data-target="#modal-default-data-fisik" data-id="{{ $capaian->id }}" style="width: 10px; height: 10px; 
+                    display: flex; justify-content: center; align-items: center;">
                       <i class="fa fa-plus"></i>
                     </a>
-                    <a class="btn btn-app" href="{{ route('detailInfo', $capaian->id) }}" style="width: 10px; height: 10px; display: flex; justify-content: center; align-items: center;">
-                      <i class="fa fa-info"></i>
+                    <a class="btn btn-app" href="{{ route('detailInfo', $capaian->id) }}" style="width: 10px; height: 10px; 
+                    display: flex; justify-content: center; align-items: center;">
+                      <i class="fa fa-info"></i> 
                     </a>
                   </div>
                 </td>
